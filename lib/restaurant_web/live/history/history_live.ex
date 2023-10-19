7f7a766ba3d3cf838f0db90bda2b7fa3c14f553e
abassGarane <thelibertinewreck@gmail.com>
2023-10-19 14:45:34 +0300

    route history live view without refresh

--- lib/restaurant_web/live/history/history_live.ex
+++ lib/restaurant_web/live/history/history_live.ex
@@ -1,8 +1,3 @@
 defmodule RestaurantWeb.HistoryLive do
   use RestaurantWeb, :live_view
-
-  def mount(_params, _session, socket) do
-    socket = assign(socket, :count, 10)
-    {:ok, socket}
-  end
 end
